0c9f15d7c0e035d9ff2f4a54e0db709ebdc545a8
an17653 <andrei.nikiforov@stud.th-deg.de>
2023-10-29 16:29:46 +0100

    Added tic_tac_toe_ml.py

---
+++ Reinforcement Learning/tic_tac_toe_ml.py
@@ -0,0 +1,47 @@
+# Placement information
+
+# | _ | _ | _ |
+# | _ | _ | _ | = '_________'
+# | _ | _ | _ |
+
+# PLacement index
+# | 0 | 1 | 2 |
+# | 3 | 4 | 5 | = '012345678'
+# | 6 | 7 | 9 |
+
+# | X | _ | O |
+# | _ | X | _ | = 'X_O_X_O_X'
+# | O | _ | X |
+
+# | O | _ | O |
+# | _ | X | _ | = 'O_O_X_X_X'
+# | X | _ | X |
+
+
+def win_check(board, player):
+    # Rows and Columns
+    for i in range(3):
+        # Rows
+        if all(board[i * 3 + j] == player for j in range(3)):
+            return True
+        # Columns
+        if all(board[j * 3 + i] == player for j in range(3)):
+            return True
+
+    # Top-Left to Bottom-Right Diagonals
+    if all(board[i * 3 + i] == player for i in range(3)):
+        return True
+    # Bottom-Left to Top-Right Diagonals
+    if all(board[i * 3 + 2 - i] == player for i in range(3)):
+        return True
+
+    return False
+
+
+def place(board, player, position):
+    return board[:position] + player + board[position + 1:]
+
+
+def initialise_board():
+    return '_________'
+
