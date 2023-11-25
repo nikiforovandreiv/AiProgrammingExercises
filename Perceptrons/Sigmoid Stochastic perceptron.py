6b84a1fdda49e2b6f7c42a63be2c871894eeea93
Sm4llpuppy <nucklearduk0@gmail.com>
2023-11-25 23:19:00 +0100

    add Sigmoid Stochastic perceptron

--- Perceptrons/Sigmoid Stochastic perceptron.py
+++ Perceptrons/Sigmoid Stochastic perceptron.py
@@ -1,17 +1,20 @@
 import random
 import itertools
 import numpy as np
+import math
 from sklearn.metrics import accuracy_score
 
 rng = np.random.default_rng(123)
 
-threshold = lambda x: 1 if x > 0 else 0
+sigmoid = lambda x: 1 / (1 + math.e ** -x)
 
 
 class Perceptron:
-    def __init__(self, ninp, activation=threshold):
+    def __init__(self, ninp, activation=sigmoid):
         self.ninp = ninp
         self.weights = rng.random(ninp + 1)
+        for i in range(ninp + 1):
+            self.weights[i] /= 10
         self.activation = activation
 
     def out(self, xs):
@@ -19,20 +22,21 @@ class Perceptron:
         o = self.activation(S)
         return o
 
-    def train(self, trainset):
-        eta = 0.9
+    def train(self, trainset, batch_size=1):
+        eta = 0.5
         maxiter = 100
-        L = len(trainset)
         while maxiter > 0:
             oldweights = np.copy(self.weights)
-            for digit in rng.permutation(range(L)):
-                T = digit
-                I = list(itertools.chain.from_iterable(random.choice(trainset[digit])))
+            delta = [0 for _ in range(len(self.weights))]
+            for _ in range(batch_size):
+                T = random.choice(list(trainset.keys()))
+                I = list(itertools.chain.from_iterable(random.choice(trainset[T])))
                 R = self.out(I)
-                if T != R:
-                    self.weights[0] = self.weights[0] + eta * (T - R)
-                    for i in range(1, len(self.weights)):
-                        self.weights[i] = self.weights[i] + eta * (T - R) * I[i-1]
+                delta[0] = self.weights[0] + eta * (T - R) * R * (1 - R)
+                for i in range(1, len(delta)):
+                    delta[i] += eta * (T - R) * R * (1 - R) * I[i-1]
+            for i in range(len(self.weights)):
+                self.weights[i] += delta[i]
             if maxiter % 10 == 0:
                 print(f"new weights {self.weights}")
 
@@ -46,8 +50,10 @@ class Perceptron:
         truth = []
         for digit in range(2):
             for sample in range(len(test_set[digit])):
-                output = self.out(list(itertools.chain.from_iterable(test_set[digit][sample])))
+                b = self.out(list(itertools.chain.from_iterable(test_set[digit][sample])))
+                output = round(b)
                 target = digit
+                # print(f"Predicted: {output} \n Truth: {target}")
                 truth.append(target)
                 predicted.append(output)
         return accuracy_score(truth, predicted)
@@ -64,7 +70,7 @@ def create_dataset(size):
                 [0, 0, 1, 0],
                 [0, 0, 1, 0]]
     data = {0: [ideal[0]], 1: [ideal[1]]}
-    for _ in range(size - 1):
+    for _ in range(size - 2):
         for key in ideal.keys():
             new_digit = [[0 for _ in range(4)] for _ in range(4)]
             for i in range(4):
